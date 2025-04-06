ea6a60bc3be384848a30daa2e6371952582c47c6
Dylan-Gresham <98201122+Dylan-Gresham@users.noreply.github.com>
2025-04-06 01:46:50 -0600

    Fix file check

--- show-metrics.sh
+++ show-metrics.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -f "data/benchmarks.csv" ]; then
+if [ -f "benchmarks.csv" ]; then
     uv run python -c "import pandas as pd; df = pd.read_csv('benchmarks.csv'); df = df.set_index('Unnamed: 0'); df.index.names = ['Model']; print(df.to_markdown(index=True))"
 else
     echo "Benchmarks haven't been generated yet."
